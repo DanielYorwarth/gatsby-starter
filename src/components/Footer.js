7ea1f0d9c5607ae93de7e6ce0d9b9e604708fd3e
Daniel Yorwarth <daniel@pagecreative.co.uk>
2020-01-08 17:11:07 +0000

    Footer text color change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import footerStyles from './Footer.module.css'
 const Footer = () => (
     <footer className={footerStyles.footer}>
         <div className="container text-center">
-            <span className="text-muted">Created with <img alt="heart" src="https://www.pagecreative.co.uk/wp-content/themes/pagecreative/images/heart.svg"></img> by PAGE</span>
+            <span className="text-dark">Created with <img alt="heart" src="https://www.pagecreative.co.uk/wp-content/themes/pagecreative/images/heart.svg"></img> by PAGE</span>
         </div>
     </footer>
 )
